b1e47dce174787e9eaa9b385920db27d6ace92ea
justlive1 <qq11419041@163.com>
2018-05-16 15:09:40 +0800

    增加copyright

--- snow-common-base/src/main/java/vip/justlive/common/base/constant/BaseConstants.java
+++ snow-common-base/src/main/java/vip/justlive/common/base/constant/BaseConstants.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2018 justlive1
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package vip.justlive.common.base.constant;
 
 /**
@@ -24,7 +37,7 @@ public class BaseConstants {
    * 返回实体message属性字段
    */
   public static final String RESP_MESSAGE_FIELD = "message";
-  
+
   /**
    * 返回实体success属性字段
    */
